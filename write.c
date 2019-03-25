14efe4b3151bb1b4e8587f4ca86967a74783a3ee
Piotr9923 <p-rzewnicki@o2.pl>
2019-03-25 07:38:22 +0100

    Create write.c

---
+++ write.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void wypisz(int x,int y,int *tab[])
+{
+	printf("\n");
+        printf(" ");
+for(int i=0;i<x*2;i++) printf("-");
+printf("\n");
+for(int i=0;i<y;i++)
+{
+        for(int j=0;j<x;j++)
+        {
+        
+                if(j==0) printf("|");
+                if(tab[i][j]==1)printf("X ");
+                else printf("  ");
+                if(j==x-1) printf("|");
+        }
+        printf("\n");
+}
+printf(" ");
+for(int i=0;i<x*2;i++) printf("-");
+     for(int i1=0;i1<10;i1++) printf("\n");
+
+}
