f29628d136a31ec516594b5f5fa2c883cfdac47c
Piotr9923 <p-rzewnicki@o2.pl>
2019-03-26 21:01:40 +0100

    Create errors.c

---
+++ errors.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void error_size(char a, char b,char c)
+{
+	
+	if(a=='9'&&b=='9'&&c=='9')
+	{
+		printf("Program nie moze zapisac wiecej ukladow komorek wiec konczy swoje dzialanie!!");
+		exit(0);
+	}
+	
+	
+}
