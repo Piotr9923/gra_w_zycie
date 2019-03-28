783c74c235ed5b5b738e7c68d97b61a5a8d1d146
Piotr9923 <48890235+Piotr9923@users.noreply.github.com>
2019-03-28 18:06:50 +0100

    Update file_reader.c
    Poprawiono funkcję wczytującą dane i dodano obsługę błędów

--- file_reader.c
+++ file_reader.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 void wczytaj_dane(int x,int y,int *tab[],char* nazwa)
 {
 
 FILE *fp=fopen(nazwa,"r");
-int x1,y1;
 
-while(fscanf(fp,"%d",&x1)!=EOF)
+if( fp == NULL ) error_nofoundfile(nazwa);
+char x1[100],y1[100];
+int point_number=1;
+
+
+while(fscanf(fp,"%s",&x1)!=EOF&&fscanf(fp,"%s",&y1)!=EOF)
 {
-fscanf(fp,"%d",&y1);
-        tab[y-y1][x1-1]=1;
-        
-        
-}
+if(error_point(point_number,x1,y1)==1) goto next;
+if(error_point_size(point_number, x, y, atoi(x1), atoi(y1))==1) goto next;
 
+tab[y-atoi(y1)][atoi(x1)-1]=1;
+
+next:
+point_number++;
+}
 }
