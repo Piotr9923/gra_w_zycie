7dd28cd4b60da7c619e825281329e65c0de8efed
Piotr9923 <48890235+Piotr9923@users.noreply.github.com>
2019-03-31 14:45:31 +0200

    Drobne poprawki

--- change.c
+++ change.c
@@ -3,30 +3,33 @@
 
 void check_aimtochange(int x,int y, int*grid[],int*change[],int*how_many_change)
 {
-	
+	int how_many_change1=0; 
 	for(int i=0;i<y;i++)
         {
                 for(int j=0;j<x;j++)
                 {
                       int how_many_neighbourhood=check_neighbourhood(x,y,grid,i,j);
                                           
-                        if(grid[i][j]==0&&how_many_neighbourhood==3) {change[i][j]=1;*how_many_change++;};
-                        if(grid[i][j]==1&&(how_many_neighbourhood<2||how_many_neighbourhood>3)){change[i][j]=1;*how_many_change++;}
+                        if(grid[i][j]==0&&how_many_neighbourhood==3) {change[i][j]=1;how_many_change1++;};
+                        if(grid[i][j]==1&&(how_many_neighbourhood<2||how_many_neighbourhood>3)){change[i][j]=1;how_many_change1;}
                       
                 }
         }
+           *how_many_change=how_many_change1;
 	
 }
 
 void change_aims(int x,int y, int*grid[],int*change[],int*how_many_live)
 {
+	int how_many_live1=0;
 	 for(int i=0;i<y;i++)
         {
                 for(int j=0;j<x;j++)
                 {
                         if(change[i][j]==1&&grid[i][j]==0) {grid[i][j]=1;change[i][j]=0;}
                         if(change[i][j]==1&&grid[i][j]==1) {grid[i][j]=0;change[i][j]=0;}
-                        if(grid[i][j]==1) *how_many_live++;
+                        if(grid[i][j]==1) how_many_live1++;
                 }
         }
+        *how_many_live=how_many_live1;
 }
