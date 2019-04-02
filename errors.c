14b98e2606563c7c769b0b6949eb3b5c0c1c0515
Piotr9923 <48890235+Piotr9923@users.noreply.github.com>
2019-04-02 19:44:05 +0200

    Poprawki wizualne wyświetalnych błędów

--- errors.c
+++ errors.c
@@ -8,12 +8,12 @@ void error_size(char a, char b,char c,int language)
 {
 	if(a=='9'&&b=='9'&&c=='9')
 	{
-		if(language==0)printf("The program can not write more cells, so ending its action!!");
-		if(language==1)printf("Program nie moze zapisac wiecej ukladow komorek wiec konczy swoje dzialanie!!");
-		if(language==2)printf("Das Programm kann nicht mehr Zellen schreiben und damit seine Aktion beenden!!");
-		if(language==3)printf("Le programme ne peut pas ecrire plus de cellules, mettant ainsi fin a son action!!");
-		if(language==4)printf("Il programma non puo scrivere piu celle, quindi termina la sua azione!!");
-		if(language==5)printf("El programa no puede escribir mas celdas, por lo que finaliza su accion!!");
+		if(language==0)printf("The program can not write more cells, so ending its action!!\n");
+		if(language==1)printf("Program nie moze zapisac wiecej ukladow komorek wiec konczy swoje dzialanie!!\n");
+		if(language==2)printf("Das Programm kann nicht mehr Zellen schreiben und damit seine Aktion beenden!!\n");
+		if(language==3)printf("Le programme ne peut pas ecrire plus de cellules, mettant ainsi fin a son action!!\n");
+		if(language==4)printf("Il programma non puo scrivere piu celle, quindi termina la sua azione!!\n");
+		if(language==5)printf("El programa no puede escribir mas celdas, por lo que finaliza su accion!!\n");
 		exit(0);
 	}
 }
@@ -30,12 +30,12 @@ void error_number(char*flagname,char*number,int language)
 	
 	if(ile!=dl)
 	{
-		if(language==0)printf("After %s you wrote %s. It is not a number!!",flagname,number);
-		if(language==1)printf("Po argumencie %s podales wartosc %s, ktora nie jest liczba!!",flagname,number);
-		if(language==2)printf("Nach dem %s, du %s geschreiben. Es ist keine Nummer!!",flagname,number);
-		if(language==3)printf("Apres %s tu as écrit %s. Ce n'est pas un chiffre!!",flagname,number);
-		if(language==4)printf("After %s Hai scritto %s. Non e un numero!!",flagname,number);
-		if(language==5)printf("Después de la %s que escribiste %s. No es un numero!!",flagname,number);
+		if(language==0)printf("After %s you wrote %s. It is not a number!!\n",flagname,number);
+		if(language==1)printf("Po argumencie %s podales wartosc %s, ktora nie jest liczba!!\n",flagname,number);
+		if(language==2)printf("Nach dem %s, du %s geschreiben. Es ist keine Nummer!!\n",flagname,number);
+		if(language==3)printf("Apres %s tu as écrit %s. Ce n'est pas un chiffre!!\n",flagname,number);
+		if(language==4)printf("After %s Hai scritto %s. Non e un numero!!\n",flagname,number);
+		if(language==5)printf("Después de la %s que escribiste %s. No es un numero!!\n",flagname,number);
 		
 		exit(0);
 	}
@@ -46,12 +46,12 @@ void error_number(char*flagname,char*number,int language)
 void error_nofilename(int language)
 {
 	
-	if(language==0)printf("You did not enter the data file name !! Do it using --filename!!");
-	if(language==1)printf("Nie podales nazwy pliku z danymi!! Zrob to przy uzyciu --filename !!");
-	if(language==2)printf("Sie haben den Dateinamen nicht eingegeben!! Du machst es mit --filename!!");
-	if(language==3)printf("Vous n'avez pas saisi le nom du fichier de donnees!! Faites-le en utilisant --filename!!");
-	if(language==4)printf("Non e stato immesso il nome del file di dati!! Farlo usando --filename!!");
-	if(language==5)printf("No ha introducir el nombre del archivo de datos!! Hazlo usando --filename!!");
+	if(language==0)printf("You did not enter the data file name !! Do it using --filename!!\n");
+	if(language==1)printf("Nie podales nazwy pliku z danymi!! Zrob to przy uzyciu --filename !!\n");
+	if(language==2)printf("Sie haben den Dateinamen nicht eingegeben!! Du machst es mit --filename!!\n");
+	if(language==3)printf("Vous n'avez pas saisi le nom du fichier de donnees!! Faites-le en utilisant --filename!!\n");
+	if(language==4)printf("Non e stato immesso il nome del file di dati!! Farlo usando --filename!!\n");
+	if(language==5)printf("No ha introducir el nombre del archivo de datos!! Hazlo usando --filename!!\n");
 
 	
 	exit(0);
@@ -61,11 +61,11 @@ void error_nofilename(int language)
 void error_nofoundfile(char*name,int language)
 {
 	if(language==0)printf("I can not open the file: %s!!",name);
-	if(language==1)printf("Nie moge otworzyc pliku o nazwie: %s!!",name);
-	if(language==2)printf("Ich kann die Datei nicht offnen: %s!!",name);
-	if(language==3)printf("Je ne peux pas ouvrir le fichier: %s!!",name);
-	if(language==4)printf("Non riesco ad aprire il file: %s!!",name);
-	if(language==5)printf("No puedo abrir el archivo: %s!!",name);
+	if(language==1)printf("Nie moge otworzyc pliku o nazwie: %s!\n!",name);
+	if(language==2)printf("Ich kann die Datei nicht offnen: %s!!\n",name);
+	if(language==3)printf("Je ne peux pas ouvrir le fichier: %s!!\n",name);
+	if(language==4)printf("Non riesco ad aprire il file: %s!!\n",name);
+	if(language==5)printf("No puedo abrir el archivo: %s!!\n",name);
 	
 	exit(0);
 	
@@ -99,12 +99,12 @@ void error_number_size(char*name,int number,int min, int mak,int language)
 {
 	if(number<min||number>mak)
 	{
-		if(language==0)printf("In %s number is not an element of interval <%d,%d>!!",name,min,mak);
-		if(language==1)printf("W %s podana liczba nie nalezy do przedzialu <%d,%d>!!",name,min,mak);
-		if(language==2)printf("Im %s Zahl ist kein intervall Element <%d,%d>!!",name,min,mak);
-		if(language==3)printf("En %s nombre n\'est pas un élément de l\'intervalle <%d,%d>!!",name,min,mak);
-		if(language==4)printf("In %s numero non e un elemento di intervallo <%d,%d>!!",name,min,mak);
-		if(language==5)printf("En %s numero no es un elemento de intervalo <%d,%d>!!",name,min,mak);
+		if(language==0)printf("In %s number is not an element of interval <%d,%d>!!\n",name,min,mak);
+		if(language==1)printf("W %s podana liczba nie nalezy do przedzialu <%d,%d>!!\n",name,min,mak);
+		if(language==2)printf("Im %s Zahl ist kein intervall Element <%d,%d>!!\n",name,min,mak);
+		if(language==3)printf("En %s nombre n\'est pas un élément de l\'intervalle <%d,%d>!!\n",name,min,mak);
+		if(language==4)printf("In %s numero non e un elemento di intervallo <%d,%d>!!\n",name,min,mak);
+		if(language==5)printf("En %s numero no es un elemento de intervalo <%d,%d>!!\n",name,min,mak);
 		exit(0);
 	}	
 }
@@ -117,10 +117,10 @@ int error_point_size(int number,int maxx,int maxy,int x,int y,int language)
 	{
 		if(language==0)printf("Point %d.: (%d,%d) is not an element of interval:  x=<1,%d>, y=<1,%d>. It was not loaded!!\n",number,x,y,maxx,maxy);
 		if(language==1)printf("Punkt %d. posiada wsporzedne (%d,%d), ktore nie naleza do przdzialu: x=<1,%d>, y=<1,%d>. Nie zostal wczytany!!\n",number,x,y,maxx,maxy);
-		if(language==2)printf("Punkt %d.: (%d,%d) ist kein intervall Element: x=<1,%d>, y=<1,%d>. Es war nicht geladen!! ",number,x,y,maxx,maxy);
-		if(language==3)printf("Le point %d.: (%d,%d) n'est pas un élément de l'intervalle: x=<1,%d>, y=<1,%d>. Il n'a pas ete charge!!",number,x,y,maxx,maxy);
-		if(language==4)printf("Il punto %d.: (%d,%d) non e un elemento di intervallo: x=<1,%d>, y=<1,%d>. Non e stato caricato!!",number,x,y,maxx,maxy);
-		if(language==5)printf("El punto %d.: (%d,%d) numero no es un elemento de intervalo: x=<1,%d>, y=<1,%d>. No estaba cargada!!",number,x,y,maxx,maxy);
+		if(language==2)printf("Punkt %d.: (%d,%d) ist kein intervall Element: x=<1,%d>, y=<1,%d>. Es war nicht geladen!!\n",number,x,y,maxx,maxy);
+		if(language==3)printf("Le point %d.: (%d,%d) n'est pas un élément de l'intervalle: x=<1,%d>, y=<1,%d>. Il n'a pas ete charge!!\n",number,x,y,maxx,maxy);
+		if(language==4)printf("Il punto %d.: (%d,%d) non e un elemento di intervallo: x=<1,%d>, y=<1,%d>. Non e stato caricato!!\n",number,x,y,maxx,maxy);
+		if(language==5)printf("El punto %d.: (%d,%d) numero no es un elemento de intervalo: x=<1,%d>, y=<1,%d>. No estaba cargada!!\n",number,x,y,maxx,maxy);
 	
 		return 1;
 	}
@@ -130,12 +130,12 @@ int error_point_size(int number,int maxx,int maxy,int x,int y,int language)
 
 void error_falgargument(char*flagname,int language)
 {
-		if(language==0)printf("After %s you did not enter the vlaue!!",flagname);
-		if(language==1)printf("Po argumencie %s nie podales wartosci!!",flagname);
-		if(language==2)printf("Nach dem %s Sie haben den Wert nicht eingegeben!!",flagname);
-		if(language==3)printf("Apres %s vous n'avez pas entre de valeur!!",flagname);
-		if(language==4)printf("After %s non hai inserito un valore!!",flagname);
-		if(language==5)printf("Después de la %s No has introducido un valor!!",flagname);
+		if(language==0)printf("After %s you did not enter the vlaue!!\n",flagname);
+		if(language==1)printf("Po argumencie %s nie podales wartosci!!\n",flagname);
+		if(language==2)printf("Nach dem %s Sie haben den Wert nicht eingegeben!!\n",flagname);
+		if(language==3)printf("Apres %s vous n'avez pas entre de valeur!!\n",flagname);
+		if(language==4)printf("After %s non hai inserito un valore!!\n",flagname);
+		if(language==5)printf("Después de la %s No has introducido un valor!!\n",flagname);
 		 	
 		 	exit(0);
 }
